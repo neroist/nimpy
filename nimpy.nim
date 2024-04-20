f029ff494149f79ba5ce6c8912d4be029f98a7ab
Jazz <pmpiga1@gmail.com>
2024-04-19 23:18:43 -0400

    remove errors from `distinct pointer` in nimpy.nim

--- nimpy.nim
+++ nimpy.nim
@@ -235,7 +235,7 @@ proc updateStackBottom() {.inline.} =
         setupForeignThreadGC()
 
 proc pythonException(e: ref Exception): PPyObject =
-  let err = pyLib.PyErr_NewException(cstring("nimpy" & "." & $(e.name)), pyLib.NimPyException, nil)
+  let err = pyLib.PyErr_NewException(cstring("nimpy" & "." & $(e.name)), pyLib.NimPyException, PPyObject nil)
   decRef err
   let errMsg: string =
     when compileOption("stackTrace"):
@@ -282,7 +282,7 @@ proc initModuleTypes[PyTypeObj](p: PPyObject, m: var PyModuleDesc) =
     discard pyLib.PyModule_AddObject(p, m.iterators[i].name, typ)
 
 
-  pyLib.NimPyException = pyLib.PyErr_NewException("nimpy.NimPyException", nil, nil)
+  pyLib.NimPyException = pyLib.PyErr_NewException("nimpy.NimPyException", PPyObject nil, PPyObject nil)
   discard pyLib.PyModule_AddObject(p, "NimPyException", pyLib.NimPyException)
 
 proc initModule2(m: var PyModuleDesc) =
@@ -294,7 +294,7 @@ proc initModule2(m: var PyModuleDesc) =
   if Py_InitModule4.isNil:
     Py_InitModule4 = cast[type(Py_InitModule4)](pyLib.module.symAddr("Py_InitModule4_64"))
   if not Py_InitModule4.isNil:
-    let py = Py_InitModule4(m.name, addr m.methods[0], m.doc, nil, PYTHON_ABI_VERSION)
+    let py = Py_InitModule4(m.name, addr m.methods[0], m.doc, PPyObject nil, PYTHON_ABI_VERSION)
     initModuleTypes[PyTypeObject3Obj](py, m) # Why does PyTypeObject3Obj work here and PyTypeObject2Obj does not???
 
 proc initPyModule(p: ptr PyModuleDef, m: var PyModuleDesc) {.inline.} =
@@ -829,7 +829,7 @@ proc callObjectAux(callable: PPyObject, args: openArray[PPyObject], kwargs: open
     discard pyLib.PyTuple_SetItem(argTuple, i, v)
     # No decRef here. PyTuple_SetItem "steals" the reference to v
 
-  var argDict: PPyObject = nil
+  var argDict: PPyObject = PPyObject nil
   if kwargs.len != 0:
     argDict = pyLib.PyDict_New()
     for v in kwargs:
@@ -984,8 +984,8 @@ proc super*(self: PyObject): PyObject {.gcsafe.} =
   incRef(self)
   discard pyLib.PyTuple_SetItem(superArgs, 1, self)
 
-  let res = pyLib.PyType_GenericNew(pyLib.PySuper_Type, superArgs, nil)
-  discard cast[PPyObject](res.to(PyObjectObj).ob_type).to(PyTypeObject3Obj).tp_init(res, superArgs, nil)
+  let res = pyLib.PyType_GenericNew(pyLib.PySuper_Type, superArgs, PPyObject nil)
+  discard cast[PPyObject](res.to(PyObjectObj).ob_type).to(PyTypeObject3Obj).tp_init(res, superArgs, PPyObject nil)
   decRef(superArgs)
 
   newPyObjectConsumingRef(res)
